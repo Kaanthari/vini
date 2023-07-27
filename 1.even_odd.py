3220d60d95c7487d17c515ed93f0ff94a9250c26
Kaanthari <125707665+Kaanthari@users.noreply.github.com>
2023-07-27 22:17:41 +0530

    uploaded even odd

---
+++ 1.even_odd.py
@@ -0,0 +1,23 @@
+#Check an entered number is odd or even
+'''
+Analysis:
+=============
+    Functional Analysis:
+    -------------------------
+        - Get the string
+        - check whether the reminder is zero
+
+    Technical Analysis:
+    =======================
+        - State : string
+        behaviour = operators (==)
+                    DM - if,else
+
+'''
+##state
+# x=int(input("Enter a number: "))
+##behaviour
+# if x%2==0:
+#     print("The value entered is even")
+# else:
+#     print("The value entered is odd")
